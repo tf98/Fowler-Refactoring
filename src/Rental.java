4670df3b2056be5d1470740a5dbca7e2086f0e4d
tf98 <tobiasfritz@outlook.de>
2019-06-18 14:42:05 +0200

    Move amountFor from Customer to Rental

--- src/Rental.java
+++ src/Rental.java
@@ -14,4 +14,24 @@ class Rental {
     public Movie getMovie() {
         return movie;
     }
+
+    double getCharge() {
+        double charge = 0;
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                charge += 2;
+                if (daysRented > 2)
+                    charge += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                charge += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                charge += 1.5;
+                if (daysRented > 3)
+                    charge += (daysRented - 3) * 1.5;
+                break;
+        }
+        return charge;
+    }
 }
